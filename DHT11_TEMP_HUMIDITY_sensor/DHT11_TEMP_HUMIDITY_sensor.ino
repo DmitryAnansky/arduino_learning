326c753a9cec8c7205f717e0f13809b675422700
Dima Ananskyi <starkandtesla@gmail.com>
2019-08-04 01:46:24 +0300

    dht11 temperature and humidity sensor

--- DHT11_TEMP_HUMIDITY_sensor/DHT11_TEMP_HUMIDITY_sensor.ino
+++ DHT11_TEMP_HUMIDITY_sensor/DHT11_TEMP_HUMIDITY_sensor.ino
@@ -1,9 +1,48 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "DHT.h"
+#define DHTPIN 4
+#define DHTTYPE DHT11
 
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHT11 testing start!");
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(2000);
 
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  
+  float humidity = dht.readHumidity();
+  float celsius = dht.readTemperature();
+  float fahrenheit = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(humidity) || isnan(celsius) || isnan(fahrenheit)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(fahrenheit, humidity);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(celsius, humidity, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(celsius);
+  Serial.print(" *C ");
+  Serial.print(fahrenheit);
+  Serial.println(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C / ");
+  Serial.print(hif);
+  Serial.println(" *F");
+  Serial.println("--------------------------");
 }
