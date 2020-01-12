6eefe31c749b979d848e6fab26c3582bfa62ac7f
danansky <starkandtesla@gmail.com>
2020-01-12 21:21:45 +0200

    Set servo degree

---
+++ Robot Mechanical Arm Kit/set_80_degrees_servo/set_80_degrees_servo.ino
@@ -0,0 +1,22 @@
+int servoPin = A0;
+int servoAngle;
+int pulseWidth;
+
+void servoPulse(int servoPin, int angle) {
+    //Converts the Angle to a pulse width value of 500 - 2480
+    pulseWidth = (11*angle) + 500;
+
+    digitalWrite(servoPin, HIGH);
+    delayMicroseconds(pulseWidth);
+    digitalWrite(servoPin, LOW);
+    delay(20 - pulseWidth/1000);
+  }
+
+void setup() {
+  pinMode(servoPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  servoPulse(servoPin,80);
+}
